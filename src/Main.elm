cf4682266e9ccc682c665813d6875e5d71a06780
Julian Jelfs <julian.jelfs@gmail.com>
2018-09-11 21:33:25 +0100

    basic json parser complete

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (JValue(..), createTuple, jsonParser, nameParser, nameValueParser, parse, parseArray, parseObject)
+module Main exposing (JValue(..), alphaParser, createTuple, jsonParser, nameValueParser, parse, parseArray, parseObject, sampleJson, stringify)
 
 import Char
 import Parser as P exposing ((|.), (|=))
@@ -20,6 +20,8 @@ jsonParser =
         [ parseObject
         , parseArray
         , parseInt
+        , parseFloat
+        , parseString
         ]
 
 
@@ -28,13 +30,29 @@ parseInt =
     P.map JInt P.int
 
 
-nameParser : P.Parser String
-nameParser =
-    P.variable
-        { start = \_ -> True
-        , inner = Char.isAlphaNum
-        , reserved = Set.empty
-        }
+parseFloat : P.Parser JValue
+parseFloat =
+    P.map JFloat P.float
+
+
+parseString : P.Parser JValue
+parseString =
+    P.succeed JString
+        |. P.symbol "\""
+        |= alphaParser
+        |. P.symbol "\""
+
+
+stringVariable =
+    { start = \_ -> True
+    , inner = \c -> Char.isAlphaNum c || c == '-' || c == '.' || c == '/'
+    , reserved = Set.empty
+    }
+
+
+alphaParser : P.Parser String
+alphaParser =
+    P.variable stringVariable
 
 
 createTuple : a -> b -> ( a, b )
@@ -46,7 +64,7 @@ nameValueParser : P.Parser ( String, JValue )
 nameValueParser =
     P.succeed createTuple
         |. P.symbol "\""
-        |= nameParser
+        |= alphaParser
         |. P.symbol "\""
         |. P.symbol ":"
         |. P.spaces
@@ -86,3 +104,35 @@ parseArray =
 parse : String -> Result (List P.DeadEnd) JValue
 parse =
     P.run jsonParser
+
+
+stringify : JValue -> String
+stringify val =
+    case val of
+        JNull ->
+            "null"
+
+        JInt i ->
+            String.fromInt i
+
+        JString str ->
+            "\"" ++ str ++ "\""
+
+        JFloat f ->
+            String.fromFloat f
+
+        JArray arr ->
+            "[" ++ (arr |> List.map stringify |> String.join ", ") ++ "]"
+
+        JObject obj ->
+            "{"
+                ++ (obj
+                        |> List.map (\( str, jval ) -> "\"" ++ str ++ "\"" ++ ": " ++ stringify jval)
+                        |> String.join ", "
+                   )
+                ++ "}"
+
+
+sampleJson : String
+sampleJson =
+    "{\"type\": \"application\", \"source-directories\": [ \"src\" ], \"elm-version\": \"0.19.0\",  \"dependencies\": { \"direct\": { \"elm/browser\": \"1.0.0\",  \"elm/core\": \"1.0.0\", \"elm/html\": \"1.0.0\", \"elm/parser\": \"1.1.0\" }, \"indirect\": { \"elm/json\": \"1.0.0\", \"elm/time\": \"1.0.0\", \"elm/url\": \"1.0.0\", \"elm/virtual-dom\": \"1.0.0\" } }, \"test-dependencies\": { \"direct\": {}, \"indirect\": {}}}"
